9462d5c2be49c0b23c95299e86dfdb1a51bbc8c1
Emmanuel BOTROS <emmanuel.botros@gmail.com>
2013-12-05 12:40:47 +0100

    refacto

--- main.cpp
+++ main.cpp
@@ -1,74 +1,77 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-using namespace std;
 #include <sstream>
 #include <noise/noise.h>
 #include "noiseutils.h"
 
+using namespace std;
 using namespace noise;
 
-int main (int argc, char** argv)
-{
+int main (int argc, char** argv) {
 
+  // load modules
   module::RidgedMulti mountainTerrain;
   module::Billow baseFlatTerrain;
   module::ScaleBias flatTerrain;
+  module::Perlin terrainType;
+  module::Select finalTerrain;
 
+  // load utils
   utils::NoiseMap heightMap;
-  utils::NoiseMapBuilderPlane heightMapBuilder;
+  utils::NoiseMapBuilderPlane heightMapBuilder;      
+  utils::RendererImage renderer;
+  utils::Image image;
+  utils::WriterBMP writer;
   
+  // base flat terrain (module Billow)
   baseFlatTerrain.SetFrequency (2.0);
   flatTerrain.SetSourceModule (0, baseFlatTerrain);
+  flatTerrain.SetScale (0.125);  
+  flatTerrain.SetBias (-0.75);
 
+  // get user arguments
   int tilesidelenght = atoi(argv[1]);
   int tilexnumber = atoi(argv[2]);
 
-  flatTerrain.SetScale (0.125);
-  flatTerrain.SetBias (-0.75);
-
-  module::Perlin terrainType;
+  // terrain type (module Perlin)
   terrainType.SetFrequency (10.0);
   terrainType.SetPersistence (0);
 
-
-  module::Select finalTerrain;
-
+  // terrain source composer (module Select)
   finalTerrain.SetSourceModule (0, flatTerrain);
   finalTerrain.SetSourceModule (1, mountainTerrain);
   finalTerrain.SetControlModule (terrainType);
-
   finalTerrain.SetEdgeFalloff (1.25);
 
+  // set builder source
   heightMapBuilder.SetSourceModule (finalTerrain);
-
   heightMapBuilder.SetDestNoiseMap (heightMap);
   heightMapBuilder.SetDestSize (tilesidelenght,tilesidelenght);
+
   for (int i = 0; i < tilexnumber; ++i) {
     for (int j = 0; j < tilexnumber; ++j) {
-      float rx=i-0.00;
-      float rl=i+1.00;
-      float ry=j-0.00;
-      float rh=j+1.00;
+      std::stringstream si;
+      std::stringstream sj;
+      si << i;
+      sj << j;
+      
+      float rx=3*(i-0.00);
+      float rl=3*(i+1.00);
+      float ry=3*(j-0.00);
+      float rh=3*(j+1.00);
 
       heightMapBuilder.SetBounds (rx,rl,ry,rh);
       heightMapBuilder.Build ();
 
-      utils::RendererImage renderer;
-      utils::Image image;
       renderer.SetSourceNoiseMap (heightMap);
       renderer.SetDestImage (image);
+
       std::string result = renderer.Render ();
 
-      utils::WriterBMP writer;
-      writer.SetSourceImage (image);
-     
-      std::stringstream si;
-      si << i;
-      std::stringstream sj;
-      sj << j;
       std::string ss = std::string("images/tile_")+ si.str() +std::string("_") + sj.str() + std::string(".bmp");
-      ofstream myfile;
+
+      writer.SetSourceImage (image);
       writer.SetDestFilename(ss);
       writer.WriteDestFile ();
     }
